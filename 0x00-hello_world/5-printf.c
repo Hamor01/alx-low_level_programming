e204d62fd622b3baf93467c8addbcaeae3b3294e
Haron Hama <haronhama2019@gmail.com>
2023-02-16 01:33:53 -0800

    A C program that observes betty rules

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-int main(voi)
+/**
+*main - A program that prints a line using the  printf function
+*Return 0 (Success)
+*/
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
