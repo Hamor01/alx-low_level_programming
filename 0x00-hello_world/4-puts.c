da26d8acedcdbba74598e3e58d3437e10b8669bf
Haron Hama <haronhama2019@gmail.com>
2023-02-16 01:20:19 -0800

    First C program that observes betty rules

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf('"Programming is like building a multilingual puzzle'\n");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
