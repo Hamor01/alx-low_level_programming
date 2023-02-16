cf74fb835c2ee96485327fc13622753edfbf2e9d
Haron Hama <haronhama2019@gmail.com>
2023-02-16 02:19:11 -0800

    6 Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
  * main - A program that prints the size of various computer types
  * Return 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	char a;
 	int b;
 	long int c;
@@ -16,4 +17,4 @@ printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
 print("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 return (0);
-
+}
