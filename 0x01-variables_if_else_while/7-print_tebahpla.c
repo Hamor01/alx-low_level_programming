1e5772b26fc242c007aa4c776139945a787bcac8
Haron Hama <haronhama2019@gmail.com>
2023-02-17 13:59:54 -0800

    Smile in the mirror, a program that prints the lowercase alphabet in reverse, followed by a new line.

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - Entry point
- * Description: prints char below 10
+ * Description: prints lowercase alphabets in reverse
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10; a++)
+	for (a = 'z'; a >= 'a'; a--)
 	{
-		putchar(a + '0');
+		putchar(a);
 	}
 	putchar('\n');
 	return (0);
