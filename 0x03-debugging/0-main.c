0b1fafe314fd15d03a7cc0f0eef10dde3cbe7477
Haron Hama <haronhama2019@gmail.com>
2023-02-22 13:17:54 -0800

    Printing zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,17 +3,13 @@
 /**
  * Main - Entry point
  * Description -  Testing function positive or negative
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
-
-/*Entry point */
-
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
