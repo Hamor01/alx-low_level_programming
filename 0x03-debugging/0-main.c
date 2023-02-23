b5c5f60a684fe70b763d9c1b1dd420e48fb78664
Haron Hama <haronhama2019@gmail.com>
2023-02-22 16:48:45 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * Main - Entry point
- * Description - Testing zero value
+ * main - Test function for positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+ int i;
 
-	i = 0;
-	positive_to_negative(i);
+ i = 0;
+ positive_or_negative(i);
 
-	return (0);
+return (0);
 }
+
