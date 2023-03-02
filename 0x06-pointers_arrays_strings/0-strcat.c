4daa269df20a53a6bacb7ef71cc2c76310ad51a1
Haron Hama <haronhama2019@gmail.com>
2023-03-02 00:51:05 -0800

    Concatenating two strings, dest and src

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * -strcat - concatenates two strings
+ *  @dest: input value
+ *  @src: input value
+ *  Author: Hamor
+ *
+ *  Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
