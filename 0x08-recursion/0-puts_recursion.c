d87aac53a26447517e3ebc69c59db818be2f850c
Haron Hama <haronhama2019@gmail.com>
2023-03-08 14:26:19 -0800

    PutsRecursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -16,13 +16,13 @@ void _puts_recursion(char *s);
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(++s);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(++s);
+}
+else
+{
+_putchar('\n');
+}
 }
