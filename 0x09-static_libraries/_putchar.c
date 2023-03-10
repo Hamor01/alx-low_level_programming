4e9e74b915c522ca714ab7f06295bda742f8c144
Haron Hama <haronhama2019@gmail.com>
2023-03-10 14:04:19 -0800

    some changes

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Writes the character c to std out
+ * Author: Hamor
+ * @c: The character to print
+ *
+ * Return: On Success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+        return(write(1,&c,1));
+}
